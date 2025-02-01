dd517a1c54b516c25ba7e1c19dbeb2e03343460a
BillMcPhil <125710492+BillMcPhil@users.noreply.github.com>
2025-02-01 14:34:18 -0500

    Added http request for lat and long

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
+import json
+import requests
 
-API_KEY = #API KEY GOES HERE
+API_KEY = #API key goes here
 app = Flask(__name__, template_folder="../client/public", static_folder="../client/src")
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})  # Enable CORS for /api/*
 
@@ -13,6 +15,15 @@ def home():
 @app.route('/api/test', methods=['GET'])
 def api_test():
     return jsonify({"data": "HELLO"}), 200
+
+def get_coords(address: str) -> (float, float):
+    try:
+        url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={API_KEY}"
+        response = requests.get(url)
+        y = response.json()
+        return (y['results'][0]["geometry"]["location"]["lat"], y['results'][0]["geometry"]["location"]["lng"])
+    except:
+        return "Request failed"
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
