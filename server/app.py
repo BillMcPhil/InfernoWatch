189c6a9c560671f0fb996f722ef1f3e515ff9edb
SpeakBoy <nevinspeakman@outlook.com>
2025-02-01 15:24:37 -0500

    Get coords from parsed address

--- server/app.py
+++ server/app.py
@@ -1,15 +1,38 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
 from flask_cors import CORS
 import json
 import requests
+import secrets
 
-API_KEY = #API key goes here
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+API_KEY = "" #API key goes here
 app = Flask(__name__, template_folder="../client/public", static_folder="../client/src")
+foo = secrets.token_urlsafe(16)
+app.secret_key = foo
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})  # Enable CORS for /api/*
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+bootstrap = Bootstrap5(app)
+csrf = CSRFProtect(app)
+
+class AddressForm(FlaskForm):
+    address = StringField("Please type in your address :)", validators=[DataRequired(), Length(0, 60)])
+    submit = SubmitField("Submit")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    form = AddressForm()
+    message = ""
+    if form.validate_on_submit():
+        address = form.address.data
+        parsed_address = parse_address(address)
+        coords = get_coords(parsed_address)
+        print(coords)
+    return render_template("index.html", form=form)
+    
 
 
 @app.route('/api/test', methods=['GET'])
